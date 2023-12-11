aa2ff593a4dceace8871371655ff9493cc0700fc
Jude Rasmussen <jude.a.rasmussen@gmail.com>
2023-12-11 15:40:46 -0800

    Initial dark mode tests

---
+++ lightmodedarkmode.js
@@ -0,0 +1,13 @@
+function toggleDarkMode() {
+    let themeButton = document.getElementById("themeButton");
+    if (themeButton.innerHTML == "dark_mode") {
+        themeButton.innerHTML = "light_mode";
+    } else {
+        themeButton.innerHTML = "dark_mode"
+    }
+    let themeL3Elements = document.getElementsByClassName("w3-theme-l3");
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        element.className = element.className.replace("w3-theme-l3", "w3-theme-d3");
+    }
+}

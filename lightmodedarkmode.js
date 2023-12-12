744498564faf21676ef7762aa538458dbd17ac9c
Jude Rasmussen <jude.a.rasmussen@gmail.com>
2023-12-11 16:44:13 -0800

    Possible fix

--- lightmodedarkmode.js
+++ lightmodedarkmode.js
@@ -1,28 +1,48 @@
+var dark;
 function toggleContrast() {
-    let themeButton = document.getElementById("themeButton");
-    if (themeButton.innerHTML == "dark_mode") {
-        themeButton.innerHTML = "light_mode";
+    if (dark) {
+        dark = false;
+        let themeL3Elements = document.getElementsByClassName("w3-theme-l3");
+        //let themeD3Elements = document.getElementsByClassName("w3-theme-d3");
+        let themeL5Elements = document.getElementsByClassName("w3-theme-l5");
+        //let themeD5Elements = document.getElementsByClassName("w3-theme-d5");
+        for (let i = 0; i < themeL3Elements.length; i++) {
+            const element = themeL3Elements[i];
+            element.className = element.className.replace("w3-theme-l3", "w3-theme-d3");
+        }
+        //for (let i = 0; i < themeD3Elements.length; i++) {
+        //    const element = themeD3Elements[i];
+        //    element.className = element.className.replace("w3-theme-d3", "w3-theme-l3");
+        //}
+        for (let i = 0; i < themeL5Elements.length; i++) {
+            const element = themeL5Elements[i];
+            element.className = element.className.replace("w3-theme-l5", "w3-theme-d5");
+        }
+        //for (let i = 0; i < themeD5Elements.length; i++) {
+        //    const element = themeD5Elements[i];
+        //    element.className = element.className.replace("w3-theme-d5", "w3-theme-l5");
+        //}
     } else {
-        themeButton.innerHTML = "dark_mode"
-    }
-    let themeL3Elements = document.getElementsByClassName("w3-theme-l3");
-    let themeD3Elements = document.getElementsByClassName("w3-theme-d3");
-    let themeL5Elements = document.getElementsByClassName("w3-theme-l5");
-    let themeD5Elements = document.getElementsByClassName("w3-theme-d5");
-    for (let i = 0; i < themeL3Elements.length; i++) {
-        const element = themeL3Elements[i];
-        element.className = element.className.replace("w3-theme-l3", "w3-theme-d3");
-    }
-    for (let i = 0; i < themeD3Elements.length; i++) {
-        const element = themeD3Elements[i];
-        element.className = element.className.replace("w3-theme-d3", "w3-theme-l3");
-    }
-    for (let i = 0; i < themeL5Elements.length; i++) {
-        const element = themeL5Elements[i];
-        element.className = element.className.replace("w3-theme-l5", "w3-theme-d5");
-    }
-    for (let i = 0; i < themeD5Elements.length; i++) {
-        const element = themeD5Elements[i];
-        element.className = element.className.replace("w3-theme-d5", "w3-theme-l5");
+        dark = true;
+        //let themeL3Elements = document.getElementsByClassName("w3-theme-l3");
+        let themeD3Elements = document.getElementsByClassName("w3-theme-d3");
+        //let themeL5Elements = document.getElementsByClassName("w3-theme-l5");
+        let themeD5Elements = document.getElementsByClassName("w3-theme-d5");
+        //for (let i = 0; i < themeL3Elements.length; i++) {
+        //    const element = themeL3Elements[i];
+        //    element.className = element.className.replace("w3-theme-l3", "w3-theme-d3");
+        //}
+        for (let i = 0; i < themeD3Elements.length; i++) {
+            const element = themeD3Elements[i];
+            element.className = element.className.replace("w3-theme-d3", "w3-theme-l3");
+        }
+        //for (let i = 0; i < themeL5Elements.length; i++) {
+        //    const element = themeL5Elements[i];
+        //    element.className = element.className.replace("w3-theme-l5", "w3-theme-d5");
+        //}
+        for (let i = 0; i < themeD5Elements.length; i++) {
+            const element = themeD5Elements[i];
+            element.className = element.className.replace("w3-theme-d5", "w3-theme-l5");
+        }
     }
 }

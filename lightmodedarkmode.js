e87d43f9dfe5be51c62d4b3846fed0d71f25ce2b
Jude Rasmussen <jude.a.rasmussen@gmail.com>
2023-12-11 17:14:56 -0800

    Testing

--- lightmodedarkmode.js
+++ lightmodedarkmode.js
@@ -1,50 +1,16 @@
-var dark = false;
 function toggleContrast() {
-    if (!dark) {
-        dark = true;
+    if (!window.localStorage.getItem("darkMode")) {
         document.getElementById("theme-button").innerHTML = "light_mode";
-        let themeL3Elements = document.getElementsByClassName("w3-theme-l3");
-        //let themeD3Elements = document.getElementsByClassName("w3-theme-d3");
-        let themeL5Elements = document.getElementsByClassName("w3-theme-l5");
-        //let themeD5Elements = document.getElementsByClassName("w3-theme-d5");
-        for (let i = 0; i < themeL3Elements.length; i++) {
-            const element = themeL3Elements[i];
-            element.className = element.className.replace("w3-theme-l3", "w3-theme-d3");
-        }
-        //for (let i = 0; i < themeD3Elements.length; i++) {
-        //    const element = themeD3Elements[i];
-        //    element.className = element.className.replace("w3-theme-d3", "w3-theme-l3");
-        //}
-        for (let i = 0; i < themeL5Elements.length; i++) {
-            const element = themeL5Elements[i];
-            element.className = element.className.replace("w3-theme-l5", "w3-theme-d5");
-        }
-        //for (let i = 0; i < themeD5Elements.length; i++) {
-        //    const element = themeD5Elements[i];
-        //    element.className = element.className.replace("w3-theme-d5", "w3-theme-l5");
-        //}
+        window.localStorage.setItem("darkMode", "true");
     } else {
-        dark = false;
+        window.localStorage.setItem("darkMode", "false")
         document.getElementById("theme-button").innerHTML = "dark_mode";
-        //let themeL3Elements = document.getElementsByClassName("w3-theme-l3");
-        let themeD3Elements = document.getElementsByClassName("w3-theme-d3");
-        //let themeL5Elements = document.getElementsByClassName("w3-theme-l5");
-        let themeD5Elements = document.getElementsByClassName("w3-theme-d5");
-        //for (let i = 0; i < themeL3Elements.length; i++) {
-        //    const element = themeL3Elements[i];
-        //    element.className = element.className.replace("w3-theme-l3", "w3-theme-d3");
-        //}
-        for (let i = 0; i < themeD3Elements.length; i++) {
-            const element = themeD3Elements[i];
-            element.className = element.className.replace("w3-theme-d3", "w3-theme-l3");
-        }
-        //for (let i = 0; i < themeL5Elements.length; i++) {
-        //    const element = themeL5Elements[i];
-        //    element.className = element.className.replace("w3-theme-l5", "w3-theme-d5");
-        //}
-        for (let i = 0; i < themeD5Elements.length; i++) {
-            const element = themeD5Elements[i];
-            element.className = element.className.replace("w3-theme-d5", "w3-theme-l5");
+    }
+    let allElements = document.querySelectorAll("*");
+    for (let i = 0; i < allElements.length; i++) {
+        const classes = allElements[i].className;
+        if (classes.includes("w3-theme-l3")) {
+            allElements[i].className = classes.replace("w3-theme-l3", "w3-theme-d3")
         }
     }
 }
